12daafe2ac0f809202f694976f67851d3913d656
shadiy <lakizvezdas@gmail.com>
2025-07-30 20:39:14 +0200

    added subsystem windows to build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-clang src/main.c libs/argparse/argparse.c -o notify-send.exe  -luser32 -lshell32 -Wl,/ENTRY:mainCRTStartup
+clang src/main.c libs/argparse/argparse.c -o notify-send.exe  -luser32 -lshell32 -Wl,/SUBSYSTEM:WINDOWS,/ENTRY:mainCRTStartup
 
